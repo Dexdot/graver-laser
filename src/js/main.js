5adebd5c9236f0993e4e5fad511833da749c3722
Dexdot <dexdotapb@gmail.com>
2017-09-27 17:55:45 +0300

    about adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
-  // Menu navigation script (toggling)
+// Menu navigation script (toggling)
     $('.hamburger, .nav__link').click(function() {
       $('.hamburger').toggleClass('is-active');
       $('.nav').toggleClass('__active');
     });
 
+// Smooth scroll
+    $("a[href*='#']").on('click', function(event) {
+      event.preventDefault();
+      var target = $(this).attr('href');
+      var top = $(target).offset().top;
+      $('html, body').animate({
+        scrollTop: top},
+        1000)
+    });
 
+// Tabs
     var tab = $('.pgtn__item');
       tab.click(function() {
         $('.pgtn__line').css('background-color', '#bdbdbd');
         var target = $(this).attr('data-target');
         $(this).children().css('background-color', '#ff8000');
         $('.hw__slide').hide();
-        $('.hw__slide').fadeOut(300);
-        $('.' + target).fadeIn(300);
+        $('.hw__slide').fadeOut(600);
+        $('.' + target).fadeIn(600);
       });
 
-// Smooth scroll
-    // $("a[href*='#']").on('click', function(event) {
-    //   event.preventDefault();
-    //   var target = $(this).attr('href');
-    //   var top = $(target).offset().top;
-    //   $('html, body').animate({
-    //     scrollTop: top},
-    //     1300)
-    // });
+
 });
