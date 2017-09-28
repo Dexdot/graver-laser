593941ddd6dc93dd7d27136088ae31ae26ca8783
Dexdot <dexdotapb@gmail.com>
2017-09-29 01:21:44 +0300

    started services w/ siema

--- src/js/main.js
+++ src/js/main.js
@@ -27,4 +27,50 @@ $(document).ready(function() {
     });
     $('.slick-dots li button').html('<span class="pgtn__line"></span>');
 
+// Service slide height
+    var siemaSlideHeight = +$('.service__info').outerHeight();
+    $('.service__slide').height(siemaSlideHeight);
+    window.addEventListener('resize', function () {
+      var siemaSlideHeight = +$('.service__info').outerHeight();
+      $('.service__slide').height(siemaSlideHeight);
+    });
+
 });
+
+// Siema
+    var mySiema = new Siema({
+      selector: '.service__slider',
+      duration: 800,
+      easing: 'ease-out'
+    });
+    var siemaBtn0 = document.querySelector('.siema__btn-0');
+    var siemaBtn1 = document.querySelector('.siema__btn-1');
+    var siemaBtn2 = document.querySelector('.siema__btn-2');
+    var siemaBtn3 = document.querySelector('.siema__btn-3');
+    var siemaBtn4 = document.querySelector('.siema__btn-4');
+
+    siemaBtn0.addEventListener('click', function () {
+      $('.siema__btn .pgtn__line').css('background', '#fff');
+      $(this).children().css('background', '#ff8000');
+      return mySiema.goTo(0);
+    });
+    siemaBtn1.addEventListener('click', function () {
+      $('.siema__btn .pgtn__line').css('background', '#fff');
+      $(this).children().css('background', '#ff8000');
+      return mySiema.goTo(1);
+    });
+    siemaBtn2.addEventListener('click', function () {
+      $('.siema__btn .pgtn__line').css('background', '#fff');
+      $(this).children().css('background', '#ff8000');
+      return mySiema.goTo(2);
+    });
+    siemaBtn3.addEventListener('click', function () {
+      $('.siema__btn .pgtn__line').css('background', '#fff');
+      $(this).children().css('background', '#ff8000');
+      return mySiema.goTo(3);
+    });
+    siemaBtn4.addEventListener('click', function () {
+      $('.siema__btn .pgtn__line').css('background', '#fff');
+      $(this).children().css('background', '#ff8000');
+      return mySiema.goTo(4);
+    });
