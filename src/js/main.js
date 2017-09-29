6525ce65ed4346811a2cd1fccf35100a25ece7f2
Dexdot <dexdotapb@gmail.com>
2017-09-29 18:36:23 +0300

    third service (bx) w/o adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -20,8 +20,9 @@ $(document).ready(function() {
       speed: 1400,
       infinite: true,
       draggable: false,
-      // autoplay: true,
-      // autoplaySpeed: 8000,
+      swipe: false,
+      autoplay: true,
+      autoplaySpeed: 8000,
       dots: true,
       arrows: false,
     });
@@ -41,6 +42,14 @@ $(document).ready(function() {
   window.addEventListener('resize', function () {
     var siemaSlideLrHeight = +$('.service__info--lr').outerHeight();
     $('.service__slider--lr .service__slide').height(siemaSlideLrHeight);
+  });
+
+// Service slide-bx height
+  var siemaSlideBxHeight = +$('.service__info--bx').outerHeight();
+  $('.service__slider--bx .service__slide').height(siemaSlideBxHeight);
+  window.addEventListener('resize', function () {
+    var siemaSlideBxHeight = +$('.service__info--bx').outerHeight();
+    $('.service__slider--bx .service__slide').height(siemaSlideBxHeight);
   });
 
 });
@@ -113,6 +122,53 @@ $(document).ready(function() {
       var siemaBtn7 = document.querySelector('.siema__btns.-lr .siema__btn-2');
       var siemaBtn8 = document.querySelector('.siema__btns.-lr .siema__btn-3');
       var siemaBtn9 = document.querySelector('.siema__btns.-lr .siema__btn-4');
+
+      siemaBtn5.addEventListener('click', function () {
+          $('.siema__btn .pgtn__line').css('background', '#fff');
+          $(this).children().css('background', '#ff8000');
+          return lrSiema.goTo(0);
+        });
+      siemaBtn6.addEventListener('click', function () {
+        $('.siema__btn .pgtn__line').css('background', '#fff');
+        $(this).children().css('background', '#ff8000');
+        return lrSiema.goTo(1);
+      });
+      siemaBtn7.addEventListener('click', function () {
+        $('.siema__btn .pgtn__line').css('background', '#fff');
+        $(this).children().css('background', '#ff8000');
+        return lrSiema.goTo(2);
+      });
+      siemaBtn8.addEventListener('click', function () {
+        $('.siema__btn .pgtn__line').css('background', '#fff');
+        $(this).children().css('background', '#ff8000');
+        return lrSiema.goTo(3);
+      });
+      siemaBtn9.addEventListener('click', function () {
+        $('.siema__btn .pgtn__line').css('background', '#fff');
+        $(this).children().css('background', '#ff8000');
+        return lrSiema.goTo(4);
+      });
+
+    /* Boxes */
+      function printSlidesBx() {
+        $('.siema__btns.-bx .pgtn__line').css('background', '#fff');
+        var sBtn = '.siema__btns.-bx .siema__btn-'+this.currentSlide;
+        $(sBtn).children().css('background', '#ff8000');
+      };
+
+      var bxSiema = new Siema({
+        selector: '.service__slider--bx',
+        duration: 800,
+        easing: 'ease-out',
+        onInit: printSlidesBx,
+        onChange: printSlidesBx
+      });
+
+      var siemaBtn5 = document.querySelector('.siema__btns.-bx .siema__btn-0');
+      var siemaBtn6 = document.querySelector('.siema__btns.-bx .siema__btn-1');
+      var siemaBtn7 = document.querySelector('.siema__btns.-bx .siema__btn-2');
+      var siemaBtn8 = document.querySelector('.siema__btns.-bx .siema__btn-3');
+      var siemaBtn9 = document.querySelector('.siema__btns.-bx .siema__btn-4');
 
       siemaBtn5.addEventListener('click', function () {
           $('.siema__btn .pgtn__line').css('background', '#fff');
