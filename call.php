4ac443abdb9f8fc6304d442f5c6959ca72737ccd
Dexdot <dexdotapb@gmail.com>
2017-10-15 20:36:16 +0300

    pre-final laser

--- call.php
+++ call.php
@@ -2,10 +2,10 @@
 $whatever=$_POST['whatever'];
 $username=$_POST['username'];
 $phone=$_POST['phone'];
-$message = "
-Имя формы: ".htmlspecialchars($whatever)."<br />
-Имя пользователя: ".htmlspecialchars($username)."<br />
-Телефон : ".htmlspecialchars($phone);
+// $message = "
+// Имя формы: ".htmlspecialchars($whatever)."<br />
+// Имя пользователя: ".htmlspecialchars($username)."<br />
+// Телефон : ".htmlspecialchars($phone);
 
 require_once('prod/vendor/phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -14,12 +14,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'd3xdot@gmail.com';
-$mail->Password = 'password';
+$mail->Username = 'graverlaserkzn@gmail.com';
+$mail->Password = 'pass';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
-$mail->setFrom('d3xdot@gmail.com', 'Обратный звонок с Лазер-Гравер');
+$mail->setFrom('graverlaserkzn@gmail.com', 'Обратный звонок с Лазер-Гравер');
 $mail->addAddress('dolotter@bk.ru');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
@@ -33,7 +33,6 @@ if(!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message has been sent';
-    // echo 'thanks.html';
 }
 
 ?>
--- cost.php
+++ cost.php
@@ -2,7 +2,7 @@
 $whatever=$_POST['whatever'];
 $username=$_POST['username'];
 $email=$_POST['email'];
-$msg=nl2br($_POST['msg']);
+$msg=$_POST['msg'];
 $message = "
 Имя формы: ".htmlspecialchars($whatever)."<br />
 Имя пользователя: ".htmlspecialchars($username)."<br />
@@ -16,12 +16,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'd3xdot@gmail.com';
-$mail->Password = 'password';
+$mail->Username = 'graverlaserkzn@gmail.com';
+$mail->Password = 'pass';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
-$mail->setFrom('d3xdot@gmail.com', 'Заявка с Лазер-Гравер');
+$mail->setFrom('graverlaserkzn@gmail.com', 'Заявка с Лазер-Гравер');
 $mail->addAddress('dolotter@bk.ru');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
@@ -35,7 +35,6 @@ if(!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message has been sent';
-    // echo 'thanks.html';
 }
 
 ?>
